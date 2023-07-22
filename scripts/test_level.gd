cc2ea8e374b1723a591c6a254dd1c6b85ed09aa4
Kyle <KyleMT94@live.com>
2023-07-22 16:06:01 +1200

    removed exit to main menu from player.gd

---
+++ scripts/test_level.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+
+var main_menu = preload("res://scenes/main_menu.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if Input.is_action_pressed("escape_to_menu"):
+			return_to_menu()
+
+func return_to_menu():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	var scene_tree = get_tree()
+	scene_tree.change_scene_to_packed(main_menu)
