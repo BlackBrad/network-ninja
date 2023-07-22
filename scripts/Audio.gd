7a0779301fe5979c4788670b3675af8697f22619
David Goosen <david@davidgoosen.kiwi>
2023-07-22 20:35:57 +1200

    Trigger horn on network pull

---
+++ scripts/Audio.gd
@@ -0,0 +1,5 @@
+extends Node
+
+func _on_seagal():
+	$Background.stop()
+	$"War Horn".play()
--- scripts/player.gd
+++ scripts/player.gd
@@ -70,6 +70,7 @@ func _physics_process(delta):
 		
 	if Input.is_action_just_pressed("trigger_seagal"):
 		get_tree().call_group("attendees", "_on_seagal")
+		get_tree().call_group("audio", "_on_seagal")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
