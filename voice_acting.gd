6fcebe5d746c0bfa83d04dce4970dfa65a272490
Bradley Black <bradley_black@me.com>
2023-07-22 20:21:41 +1200

    Add array for voice acting file names

--- voice_acting.gd
+++ voice_acting.gd
@@ -1,5 +1,26 @@
 extends Node
 
+var file_names = [
+	"do_a_buisness.mp3",
+	"lovely_weather.mp3",
+	"the_more_the_merrier.mp3",
+	"win_win_situation.mp3",
+	"take_this_offline.mp3",
+	"put_a_pin_in_it.mp3",
+	"door_always_open.mp3",
+	"if_it_is_like_as_it_is.mp3",
+	"they_are_only_words.mp3",
+	"the_data_never_lies.mp3",
+	"teamwork_dreamwork.mp3",
+	"ill_run_the_numbers.mp3",
+	"too_much_percent.mp3",
+	"we_re_going_to_raise_the_bar.mp3",
+	"monetise_it.mp3",
+	"impress_me.mp3",
+	"price_of_onions.mp3",
+	"take_an_arm.mp3",
+	"monkey_silk"
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
