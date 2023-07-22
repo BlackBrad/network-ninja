044dee60d208c9ee6209784f5efd81dda36d3fd0
Bradley Black <bradley_black@me.com>
2023-07-22 20:14:55 +1200

    Inital commit of voice acting script

---
+++ voice_acting.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func load_mp3(path):
+	var file = FileAccess.open(path, FileAccess.READ)
+	var sound = AudioStreamMP3.new()
+	sound.data = file.get_buffer((file.get_length()))
+	return sound
